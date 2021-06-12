72c7d3f2e3c86492209e6201eff9dbdfa2a86194
MelidaZ <69802296+MelidaZ@users.noreply.github.com>
2021-06-12 01:09:18 +0000

    Add files via upload

---
+++ src/MelidaZ/Main.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MelidaZ;
+
+use pocketmine\event\Listener;
+use pocketmine\event\server\QueryRegenerateEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener
+{
+	
+    private $config;
+    public function onEnable () {
+    	$this->server->getLogger()->debug("JoinAddSlot Enable");
+        $this->saveResource( "slot.yml" );
+        $this->config = new Config( $this->getDataFolder() . "slot.yml", Config::YAML );
+        $this->getServer()->getPluginManager()->registerEvents( $this, $this );
+    }
+
+    public function onDisable () {
+    	$this->server->getLogger()->debug("JoinAddSlot Disable");
+        $this->config->save();
+    }
+
+    public function RegenSlot ( QueryRegenerateEvent $ev ) {
+        $ev->setMaxPlayerCount( $ev->getPlayerCount() + $this->config->get( "JoinAddSlot" ) );
+    }
+}
